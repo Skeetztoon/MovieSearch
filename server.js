17bd31f4363627720431010e98d9e76ea77cd046
skeetztoon <1014day@gmail.com>
2023-11-29 22:06:12 +0300

    specified ip and port

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const ipAddress = '127.0.0.1';
+
+const port = '8080';
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, ipAddress, () => {
+  console.log(`Server is running at http://${ipAddress}:${port}/`);
 });
